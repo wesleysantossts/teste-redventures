a7a1688782eab0c5f46488487e2e7ce559f87724
Wesley Santos <wesley.brazil@outlook.com>
2024-06-02 20:58:13 -0300

    feat: alterando cores ao clicar nos cards

--- app/scripts/list.js
+++ app/scripts/list.js
@@ -22,22 +22,33 @@ const getProteins = async () => {
   return response;
 }
 
-const eventListener = async () => {
-  const brothData = await (await getBroths()).json();
-  const proteinData = await (await getProteins()).json();
+const createCard = async (type) => {
+  if (!type) return;
+  
+  let data;
+  let index;
+  if (type === "broth") {
+    data = await (await getBroths()).json();
+    index = 0;
+  } 
+  if (type === "protein") {
+    data = await (await getProteins()).json();
+    index = 1;
+  } 
   const slider = document.getElementsByClassName("slider-container");
-  
-  for (const broth of brothData) {
+
+  for (const item of data) {
     let el = document.createElement("div");
-    el.classList.add("slide");
+    el.classList.add(`slide-${type}`);
     el.innerHTML = `
       <div class="slide-content">
         <div>
-          <img src="${broth.imageInactive}"/>
+          <img class="icon" src="${item.imageInactive}"/>
+          <img class="icon" src="${item.imageActive}"/>
         </div>
-        <p class="title">${broth.name}</p>
-        <p class="description">${broth.description}</p>
-        <p class="price">US$ ${broth.price}</p>
+        <p class="title">${item.name}</p>
+        <p class="description">${item.description}</p>
+        <p class="price">US$ ${item.price}</p>
       </div>
       <style>
         .slide-content {
@@ -47,11 +58,16 @@ const eventListener = async () => {
           flex-direction: column;
           justify-content: center;
           align-items: center;
+          cursor: pointer;
 
           img {
             margin-bottom: 14px;
           }
-
+          
+          img:nth-child(2) {
+            display: none;
+          } 
+          
           .title {
             font-size: 20px;
             font-weight: 900;
@@ -80,65 +96,59 @@ const eventListener = async () => {
         }
       </style>
     `;
-    slider[0].appendChild(el);
+    slider[index].appendChild(el);
+    eventChangeColor();
   }
-  
-  for (const protein of proteinData) {
-    let el = document.createElement("div");
-    el.classList.add("slide");
-    el.innerHTML = `
-      <div class="slide-content">
-        <div>
-          <img src="${protein.imageInactive}"/>
-        </div>
-        <p class="title">${protein.name}</p>
-        <p class="description">${protein.description}</p>
-        <p class="price">US$ ${protein.price}</p>
-      </div>
-      <style>
-        .slide-content {
-          width: 100%;
-          padding: 40px 16px;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
+}
 
-          img {
-            margin-bottom: 14px;
-          }
+const eventListener = async () => {
+  createCard("broth");
+  createCard("protein");
+  eventChangeColor();
+}
 
-          .title {
-            font-size: 20px;
-            font-weight: 900;
-            line-height: 24px;
-            text-align: center;
-            margin-bottom: 10px;
-            color: ##1820EF;
-          }
-          
-          .description {
-            font-size: 14px;
-            font-weight: 400;
-            line-height: 16px;
-            text-align: center;
-            color: #000;
-            margin-bottom: 36px;
-          }
+const changeStyle = ({component, selector, attribute, value}) => {
+  component.querySelectorAll(selector).forEach(item => item.style[attribute] = value);
+}
 
-          .price {
-            color: #FF4E42;
-            font-size: 20px;
-            font-weight: 900;
-            line-height: 24px;
-            text-align: center;
-          }
-        }
-      </style>
-    `;
-    slider[1].appendChild(el);
-  }
-}  
+const handleClick = async (selector) => {
+  const colorBoxes = document.querySelectorAll(selector);
+  const imgSelector = document.querySelectorAll("img.icon");
+  let lastClickedBox = null;
+
+  colorBoxes.forEach(box => {
+    box.addEventListener('click', function() {
+      if (lastClickedBox) {
+        lastClickedBox.style.backgroundColor = "#FAFAED";
+        changeStyle({ component: lastClickedBox, selector: ".title", attribute: "color", value: "#1820EF" });
+        changeStyle({ component: lastClickedBox, selector: ".description", attribute: "color", value: "black" });
+        changeStyle({ component: lastClickedBox, selector: ".price", attribute: "color", value: "#FF4E42" });
+        lastClickedBox.querySelectorAll(".icon").forEach((item, index) => {
+          if (index === 0) item.style.display = "flex";
+          if (index === 1) item.style.display = "none";
+        });
+      }
+      
+      box.querySelectorAll(".icon").forEach((item, index) => {
+        if (index === 0) item.style.display = "none";
+        if (index === 1) item.style.display = "flex";
+      });
+      box.dataset.originalColor = box.style.backgroundColor;
+      box.style.backgroundColor = '#1820EF';
+      lastClickedBox = box;
+      
+      changeStyle({ component: box, selector: ".title", attribute: "color", value: "#FFF" });
+      changeStyle({ component: box, selector: ".description", attribute: "color", value: "#FFF" });
+      changeStyle({ component: box, selector: ".price", attribute: "color", value: "#FFC024" });
+    });
+  });
+}
+
+const eventChangeColor = async () => {
+  handleClick('.slide-broth');
+  handleClick('.slide-protein');
+}
+
 
 window.addEventListener('DOMContentLoaded', () => {
   eventListener();
